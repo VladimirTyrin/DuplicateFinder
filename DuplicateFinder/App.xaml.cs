79a8a789576567fc1ecc588668730262961d3748
Vladimir Tyrin <b0-0b@yandex.ru>
2018-07-18 10:12:42 +0300

    Basic setup

--- DuplicateFinder/App.xaml.cs
+++ DuplicateFinder/App.xaml.cs
@@ -1,17 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+﻿#define AUTO_LOG_WINDOW
+
+using System;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using DuplicateFinder.Managers;
+using ITCC.Logging.Core;
+using ITCC.WPF.Windows;
 
 namespace DuplicateFinder
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    // ReSharper disable once RedundantExtendsListEntry
     public partial class App : Application
     {
+        public static async Task RunOnUiThreadAsync(Action action) => await Current.Dispatcher.InvokeAsync(action);
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            try
+            {
+                LoggerManager.StartLoggers();
+                LogMessage(LogLevel.Info, "Logging started");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n" + ex.InnerException?.Message,
+                    "Startup error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Current.Shutdown();
+            }
+
+#if DEBUG && AUTO_LOG_WINDOW
+            var logWindow = new LogWindow(LoggerManager.ObservableLogger);
+            logWindow.Show();
+#endif
+            LogMessage(LogLevel.Info, "Application is ready");
+        }
+
+        private async void App_OnExit(object sender, ExitEventArgs e)
+        {
+            await LoggerManager.FinalizeLoggersAsync();
+        }
+
+        private async void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
+        {
+            LogException(LogLevel.Critical, args.Exception);
+            await Logger.FlushAllAsync();
+#if DEBUG
+            args.Handled = true;
+#else
+            Current.Shutdown();
+#endif
+        }
+
+        public static void LogMessage(LogLevel level, string message) => Logger.LogEntry("APPLICATION", level, message);
+
+        private static void LogException(LogLevel level, Exception exception) => Logger.LogException("APPLICATION", level, exception);
     }
 }
---
+++ DuplicateFinder/Managers/LoggerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using ITCC.Logging.Core;
+using ITCC.UI.Loggers;
+
+namespace DuplicateFinder.Managers
+{
+    public static class LoggerManager
+    {
+        public static void StartLoggers()
+        {
+            Logger.Level = LogLevel.Trace;
+            ObservableLogger = new ObservableLogger(10000, App.RunOnUiThreadAsync) { Level = Logger.Level };
+            Logger.RegisterReceiver(ObservableLogger, true);
+        }
+
+        public static Task FinalizeLoggersAsync()
+        {
+            App.LogMessage(LogLevel.Info, "Application is shutting down");
+            return Logger.FlushAllAsync();
+        }
+
+        public static ObservableLogger ObservableLogger { get; private set; }
+    }
+}
